47c6bbd421ea68a822f7f7c13398eeea5be63032
jerschu <89805184+jerschu@users.noreply.github.com>
2022-01-09 12:25:35 -0600

    Update heading.js

--- heading.js
+++ heading.js
@@ -48,7 +48,7 @@ function loadHeader() {
 		if(contentText != ""){
 			var alert="<html><head><style>";
 			alert+='.banner{background-color:red; display:flex;width:100%;}';
-			alert+='.alerttxt{display: inline-block; padding:10px, 10px; flex-grow: 1; color: white; font-family: "Helvetica Neue", Helvetica;}';
+			alert+='.alerttxt{display: inline-block; line-height:50px; margin-left: 20px; flex-grow: 1; color: white; font-family: "Helvetica Neue", Helvetica;}';
 			alert+='.btn{ display: inline-block background-color: red; border-style: align-items: center; solid; border-color: red; width: 50px; background:red !important; background-color:red !important; color:white !important;}';
 			alert+='</style></head><body>';
 			alert+='<div class="banner" id="alert">	<div class="alerttxt">'+contentText+'</div>';
