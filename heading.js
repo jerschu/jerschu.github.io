d37fbc9f130fe08f2d254a449b0d5cec71c7936e
jerschu <89805184+jerschu@users.noreply.github.com>
2022-08-08 18:39:01 -0500

    Update heading.js

--- heading.js
+++ heading.js
@@ -13,40 +13,40 @@ function resize() {
 }
 
 function loadHeader() {
-	var contentText=""; /*change to add alert*/
+	var contentText="test"; /*change to add alert*/
 	let names =[
 		 ["COMPUTER SCIENCE ",["CS_159"]],
-		 ["MATH ",["MA_261"]],
+		 ["MATH ",["MA_261","MA_265","MA_266"]],
 		 ["ENGINEERING ",["ENGR_133","ME_200"]],
 		 ["ECON ",["ECON_251"]]
 		];
-	 var nav='<div class="header">\n\
-		  Boiler Schubox\n\
-		  </div>\n\
-		  <div id="navbar">\n\
-		  <a href="index">Home</a>\n\
-	          <a href="directory">Directory</a>'
-	  for(var j=0;j<names.length;j++){
+	
+		var nav="";
+		nav+='<div class="topnav" id="myTopnav">\n\
+		<a href="#home">Home</a>\n\
+		<a href="#news">Directory</a>'
+		
+		for(var j=0;j<names.length;j++){
 		nav+='<div class="dropdown">\n\
-			<button class="dropbtn">\n\
-			'+names[j][0]+' \n\
-			<i class="fa fa-caret-down"></i>\n\
+			<button class="dropbtn">'+names[j][0]+'\n\
+				<i class="fa fa-caret-down"></i>\n\
 			</button>\n\
 			<div class="dropdown-content">'
-		for (var i=0;i<names[j][1].length;i++){
-
-			nav+='<a href=class?'+names[j][1][i]+'>'+(names[j][1][i].split("_")).join(' ')+'</a></br>';
-		}
-	   }
+			for(var i=0;i<names[j][1].length;i++){
+			  nav+='<a href="class?'+names[j][1][i]+'">'+(names[j][1][i].split("_")).join(' ')+'</a>'
 			
-	   nav+='</div></div>'
+			}
+		nav+='</div></div>'
+		}
+	 
+		nav+='<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="resize()">&#9776;</a></div>'
 
 	   if(contentText != ""){
 		var alert='<div class="banner" id="alert"><div class="alerttxt">'+contentText+'</div>';
-		alert+='<button class="btn" onclick="closeWindow()"><span aria-hidden="true">&times;</span></button></div>';
+		alert+='<button type="button" class="btn" onclick="closeWindow()"><span aria-hidden="true">&times;</span></button></div>';
 		nav+=alert;
 	   }
-	   nav+='<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="resize()">&#9776;</a>';
+	   
 	   nav+='</div>';	 
 	   document.getElementsByTagName("header")[0].innerHTML = nav; 
 } 
