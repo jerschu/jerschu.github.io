15995c8a356c32e0b8ac6925b0481fb3eb792d58
jerschu <89805184+jerschu@users.noreply.github.com>
2022-08-08 00:33:52 -0500

    Update heading.js

--- heading.js
+++ heading.js
@@ -3,32 +3,50 @@ function closeWindow(){
 	alt.style.display = "none";
 }
 
+function resize() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 function loadHeader() {
 	var contentText=""; /*change to add alert*/
-	let names =["CS_159","MA_261","ENGR_133","ECON_251","ME_200"];
+	let names =[
+		 ["COMPUTER SCIENCE ",["CS_159"]],
+		 ["MATH ",["MA_261"]],
+		 ["ENGINEERING ",["ENGR_133","ME_200"]],
+		 ["ECON ",["ECON_251"]]
+		];
 	 var nav='<div class="header">\n\
-				Boiler Schubox\n\
+		  Boiler Schubox\n\
 		  </div>\n\
 		  <div id="navbar">\n\
-			<a href="index">Home</a>\n\
-			<a href="directory">Directory</a>\n\
-			<div class="dropdown">\n\
-				<button class="dropbtn">\n\
-					Classes \n\
-					<i class="fa fa-caret-down"></i>\n\
-				</button>\n\
-				<div class="dropdown-content">'
-		for (var i=0;i<names.length;i++){
+		  <a href="index">Home</a>\n\
+	          <a href="directory">Directory</a>'
+	  for(var j-0;j<names.length;j++){
+		nav+='<div class="dropdown">\n\
+			<button class="dropbtn">\n\
+			'+names[j][0]+' \n\
+			<i class="fa fa-caret-down"></i>\n\
+			</button>\n\
+			<div class="dropdown-content">'
+		for (var i=0;i<names[j][1][i].length;i++){
+
+			nav+='<a href=class?'+names[j][1][i]+'>'+(names[j][1][i].split("_")).join(' ')+'</a></br>';
+		}
+	   }
 			
-			nav+='<a href=class?'+names[i]+'>'+(names[i].split("_")).join(' ')+'</a></br>';
-		}
-		nav+='</div></div>'
-	
-		if(contentText != ""){
-			var alert='<div class="banner" id="alert"><div class="alerttxt">'+contentText+'</div>';
-			alert+='<button class="btn" onclick="closeWindow()"><span aria-hidden="true">&times;</span></button></div>';
-			nav+=alert;
-		}
-		nav+='</div>';	 
-	 document.getElementsByTagName("header")[0].innerHTML = nav; 
+	   nav+='</div></div>'
+
+	   if(contentText != ""){
+		var alert='<div class="banner" id="alert"><div class="alerttxt">'+contentText+'</div>';
+		alert+='<button class="btn" onclick="closeWindow()"><span aria-hidden="true">&times;</span></button></div>';
+		nav+=alert;
+	   }
+	   nav+='<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="resize()">&#9776;</a>';
+	   nav+='</div>';	 
+	   document.getElementsByTagName("header")[0].innerHTML = nav; 
 } 
